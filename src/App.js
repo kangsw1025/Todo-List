a18059ccfc3334aefa07500b8474034a15fefd01
9ang <kangsw1025@naver.com>
2020-10-26 22:57:43 +0900

    Todo-List #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
 import React, { Component } from "react";
 import TodoListTemplate from "./component/TodoListTemplate";
-import Form from './component/Form'
+import Form from "./component/Form";
+import TodoItemList from "./component/TodoItemList";
 
 class App extends Component {
+  id = 3;
+
+  state = {
+    input: "",
+    todos: [
+      { id: 0, text: " Introduction React", checked: false },
+      { id: 1, text: " Introduction React", checked: true },
+      { id: 2, text: " Introduction React", checked: false },
+    ],
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  };
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: "",
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false,
+      }),
+    });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleCreate();
+    }
+  };
+
   render() {
-    return <TodoListTemplate form={<Form/>}>Template Complete</TodoListTemplate>;
+    const { input, todos } = this.state;
+    const { handleChange, handleCreate, handleKeyPress } = this;
+
+    return (
+      <TodoListTemplate
+        form={
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        }
+      >
+        <TodoItemList todos={todos}/>
+      </TodoListTemplate>
+    );
   }
 }
 
